2454c854c1ed64e650a734da29580366961877b4
Mohammad Akbari <mohammad.akbari@outlook.com>
2023-12-22 13:47:48 +0330

    Added soft delete

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Account extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function user()
     {
